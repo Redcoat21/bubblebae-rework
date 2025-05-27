f5bb7936a68e11410b07fcd7a3b7185b3277129e
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-05-28 02:23:54 +0700

    feat: Added implementation for UserDataSource

--- app/src/main/java/com/dahrenericsson/bubblebaerework/data/datasource/remote/supabase/UserRemoteDataSource.kt
+++ app/src/main/java/com/dahrenericsson/bubblebaerework/data/datasource/remote/supabase/UserRemoteDataSource.kt
@@ -2,25 +2,37 @@ package com.dahrenericsson.bubblebaerework.data.datasource.remote.supabase
 
 import com.dahrenericsson.bubblebaerework.data.model.UserDto
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.postgrest.from
 import javax.inject.Inject
 
 class UserRemoteDataSource @Inject constructor(private val client: SupabaseClient) {
     suspend fun getUserById(id: String): UserDto? {
-//        val req = client.from(UserDto.TABLE_NAME).select() {
-//            filter {
-//                eq(UserDto::id.name, id)
-//            }
-//        }
-//        val res = req.decodeSingleOrNull<UserDto>()
-//        return res
-        TODO("Not yet implemented")
+        val req = client.from(UserDto.TABLE_NAME).select() {
+            filter {
+                eq(UserDto::id.name, id)
+            }
+        }
+        val res = req.decodeSingleOrNull<UserDto>()
+        return res
     }
 
     suspend fun getUserByEmail(email: String): UserDto? {
-        TODO("Not yet implemented")
+        val req = client.from(UserDto.TABLE_NAME).select() {
+            filter {
+                eq(UserDto::email.name, email)
+            }
+        }
+        val res = req.decodeSingleOrNull<UserDto>()
+        return res
     }
 
     suspend fun getUserByUsername(username: String): UserDto? {
-        TODO("Not yet implemented")
+        val req = client.from(UserDto.TABLE_NAME).select() {
+            filter {
+                eq(UserDto::username.name, username)
+            }
+        }
+        val res = req.decodeSingleOrNull<UserDto>()
+        return res
     }
 }
